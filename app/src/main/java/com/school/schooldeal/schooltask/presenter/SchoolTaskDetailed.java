4d8993572ca6a27e626dcd4511b8410151d5cb5b
教科书式的机智少年 <tang.lei96@foxmail.com>
2017-02-09 23:11:28 +0800

    添加布局

--- app/src/main/java/com/school/schooldeal/schooltask/presenter/SchoolTaskDetailed.java
+++ app/src/main/java/com/school/schooldeal/schooltask/presenter/SchoolTaskDetailed.java
@@ -1,8 +1,18 @@
 package com.school.schooldeal.schooltask.presenter;
 
+import android.content.Context;
+
+import com.school.schooldeal.schooltask.view.SchoolTaskDetailedActivity;
+
 /**
  * Created by 教科书式的机智少年 on 2017/2/5.
  */
 
 public class SchoolTaskDetailed {
+    private SchoolTaskDetailedActivity detailedActivity;
+    private Context context;
+
+    public SchoolTaskDetailed (Context context){
+        this.context = context;
+    }
 }
